9604ca58e25d3527bd9111ede8a1ceb5b3541310
Cesar do Nascimento <git@cesardonascimento.com>
2025-10-05 17:18:46 -0300

    feat(seeds): create seeds for all platforms

--- src/db/scripts/seed-db.ts
+++ src/db/scripts/seed-db.ts
@@ -2,7 +2,32 @@ import { PlatformInsert } from '@/db/repositories/platform.repository';
 import { normalizeTitle } from '@/lib/games/utils';
 import { db } from '../index';
 import { gamesTable, platformsTable } from '../schema';
+import _32xGames from '../seeds/games/32x.json';
+import _3doGames from '../seeds/games/3do.json';
+import dreamcastGames from '../seeds/games/dreamcast.json';
+import gbaGames from '../seeds/games/game-boy-advance.json';
+import gbcGames from '../seeds/games/game-boy-color.json';
+import gbGames from '../seeds/games/game-boy.json';
+import ggGames from '../seeds/games/game-gear.json';
+import gcGames from '../seeds/games/gamecube.json';
+import jaguarGames from '../seeds/games/jaguar.json';
+import lynxGames from '../seeds/games/lynx.json';
+import masterSystemGames from '../seeds/games/master-system.json';
 import megaDriveGames from '../seeds/games/mega-drive.json';
+import ngpcGames from '../seeds/games/neo-geo-pocket-color.json';
+import ngGames from '../seeds/games/neo-geo.json';
+import nesGames from '../seeds/games/nes.json';
+import n64Games from '../seeds/games/nintendo-64.json';
+import ndsGames from '../seeds/games/nintendo-ds.json';
+import pcEngineGames from '../seeds/games/pc-engine.json';
+import pspGames from '../seeds/games/playstation-portable.json';
+import psxGames from '../seeds/games/psx.json';
+import saturnGames from '../seeds/games/saturn.json';
+import segaCdGames from '../seeds/games/sega-cd.json';
+import snesGames from '../seeds/games/snes.json';
+import wiiGames from '../seeds/games/wii.json';
+import wscGames from '../seeds/games/wonderswan-color.json';
+import wsGames from '../seeds/games/wonderswan.json';
 
 export async function seedDatabase() {
   const mapGames = (
@@ -184,8 +209,6 @@ export async function seedDatabase() {
       name: 'WonderSwan Color',
     };
 
-    const megadriveGames = mapGames(megadrive.id, megaDriveGames);
-
     await tx.insert(platformsTable).values(_3do);
     await tx.insert(platformsTable).values(arcade);
     await tx.insert(platformsTable).values(atarijaguar);
@@ -198,7 +221,6 @@ export async function seedDatabase() {
     await tx.insert(platformsTable).values(gc);
     await tx.insert(platformsTable).values(mastersystem);
     await tx.insert(platformsTable).values(megadrive);
-    await tx.insert(gamesTable).values(megadriveGames);
     await tx.insert(platformsTable).values(n64);
     await tx.insert(platformsTable).values(nds);
     await tx.insert(platformsTable).values(neogeo);
@@ -215,6 +237,39 @@ export async function seedDatabase() {
     await tx.insert(platformsTable).values(wii);
     await tx.insert(platformsTable).values(wonderswan);
     await tx.insert(platformsTable).values(wonderswancolor);
+
+    await tx.insert(gamesTable).values(mapGames(_3do.id, _3doGames));
+    // await tx.insert(gamesTable).values(mapGames(arcade.id, arcadeGames));
+    await tx.insert(gamesTable).values(mapGames(atarijaguar.id, jaguarGames));
+    await tx.insert(gamesTable).values(mapGames(atarilynx.id, lynxGames));
+    await tx.insert(gamesTable).values(mapGames(dreamcast.id, dreamcastGames));
+    await tx.insert(gamesTable).values(mapGames(gamegear.id, ggGames));
+    await tx.insert(gamesTable).values(mapGames(gb.id, gbGames));
+    await tx.insert(gamesTable).values(mapGames(gba.id, gbaGames));
+    await tx.insert(gamesTable).values(mapGames(gbc.id, gbcGames));
+    await tx.insert(gamesTable).values(mapGames(gc.id, gcGames));
+    await tx
+      .insert(gamesTable)
+      .values(mapGames(mastersystem.id, masterSystemGames));
+    await tx.insert(gamesTable).values(mapGames(megadrive.id, megaDriveGames));
+    await tx.insert(gamesTable).values(mapGames(n64.id, n64Games));
+    await tx.insert(gamesTable).values(mapGames(nds.id, ndsGames));
+    await tx.insert(gamesTable).values(mapGames(neogeo.id, ngGames));
+    await tx.insert(gamesTable).values(mapGames(nes.id, nesGames));
+    await tx.insert(gamesTable).values(mapGames(ngpc.id, ngpcGames));
+    await tx.insert(gamesTable).values(mapGames(pcengine.id, pcEngineGames));
+    // await tx.insert(gamesTable).values(mapGames(ps2.id, ps2Games));
+    await tx.insert(gamesTable).values(mapGames(psp.id, pspGames));
+    await tx.insert(gamesTable).values(mapGames(psx.id, psxGames));
+    await tx.insert(gamesTable).values(mapGames(saturn.id, saturnGames));
+    await tx.insert(gamesTable).values(mapGames(sega32x.id, _32xGames));
+    await tx.insert(gamesTable).values(mapGames(segacd.id, segaCdGames));
+    await tx.insert(gamesTable).values(mapGames(snes.id, snesGames));
+    await tx.insert(gamesTable).values(mapGames(wii.id, wiiGames));
+    await tx.insert(gamesTable).values(mapGames(wonderswan.id, wsGames));
+    await tx.insert(gamesTable).values(mapGames(wonderswancolor.id, wscGames));
+
+    await tx.insert(gamesTable).values(mapGames(_3do.id, _3doGames));
   });
 }
 
